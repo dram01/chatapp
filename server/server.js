c719f4edca0a88d62419d252086c5d821d4212b6
Danyl <danyloonopko@gmail.com>
2026-05-31 23:27:39 +0200

    test cors wildcard

--- server/server.js
+++ server/server.js
@@ -10,12 +10,11 @@ const authRoutes = require('./routes/auth');
 
 const app = express();
 const server = http.createServer(app);
-
-const ALLOWED_ORIGIN = 'https://chatapp-steel-eta.vercel.app';
+const ALLOWED_ORIGIN = '*';
 
 // Manual CORS headers - must be FIRST
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', ALLOWED_ORIGIN);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'OPTIONS') {
