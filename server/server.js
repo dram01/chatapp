36393d9d5bda708abe03d7a09602e5c20df386b3
Danyl <danyloonopko@gmail.com>
2026-05-31 23:37:25 +0200

    restrict cors to vercel domain

--- server/server.js
+++ server/server.js
@@ -10,11 +10,10 @@ const authRoutes = require('./routes/auth');
 
 const app = express();
 const server = http.createServer(app);
-const ALLOWED_ORIGIN = '*';
+const ALLOWED_ORIGIN = 'https://chatapp-steel-eta.vercel.app';
 
 // Manual CORS headers - must be FIRST
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'OPTIONS') {
